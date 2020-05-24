f7e8a3accd5ff8761a15dcb962741833d06a7b5f
hugofalkman <hugo@falkman.net>
2020-05-24 18:05:17 +0200

    Initial Commit

---
+++ Memorize/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by H Hugo Falkman on 24/05/2020.
+//  Copyright © 2020 H Hugo Falkman. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
